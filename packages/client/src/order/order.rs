d3bd59425354f1c6327b53498933c330f1bed52e
Julian Chen <julian@Julians-MacBook-Pro-2.local>
2022-11-05 20:24:58 -0700

    updated order and create_predicate

--- packages/client/src/order/order.rs
+++ packages/client/src/order/order.rs
@@ -1,6 +1,6 @@
 use crate::utils::builder::LimitOrder;
 use crate::utils::environment as env;
-use crate::utils::create_predicate:create_predicate;
+use crate::utils::pred;
 
 use fuels::{
     contract::predicate::Predicate,
@@ -8,15 +8,24 @@ use fuels::{
     signers::WalletUnlocked,
     tx::{Address, AssetId, Contract, Input, TxPointer, UtxoId},
 };
+
+const PREDICATE: &str = "../order-predicate/out/debug/order-predicate.bin";
 /// Gets the message to contract predicate
+pub fn get_predicate() -> (Vec<u8>, Address) {
+    let predicate_bytecode = std::fs::read(PREDICATE).unwrap();
+    let predicate_root = Address::from(*Contract::root_from_code(&predicate_bytecode));
+    (predicate_bytecode, predicate_root)
+}
+
 pub async fn create_order(
     maker: &WalletUnlocked,
     order: &LimitOrder,
     provider: &Provider,
 ) -> (Predicate, Input) {
+    pred.create_predicate();
+    pred.compile_to_bytes();
     let predicate = Predicate::load_from(PREDICATE).unwrap();
     let (predicate_bytecode, predicate_root) = get_predicate();
-
     // create the order (fund the predicate)
     let (_tx, _rec) = maker
         .transfer(
@@ -42,6 +51,19 @@ pub async fn create_order(
         predicate_data: vec![],
     };
     (predicate, predicate_coin_input)
+}
+
+pub async fn verify_balance_of_maker_and_predicate(
+    maker: &WalletUnlocked,
+    predicate: &Bech32Address,
+    asset: AssetId,
+    amount: u64,
+    provider: &Provider,
+) {
+    let balance = maker.get_asset_balance(&asset).await.unwrap();
+    let predicate_balance = provider.get_asset_balance(predicate, asset).await.unwrap();
+    assert!(balance == 0);
+    assert!(predicate_balance == amount);
 }
 
 pub async fn take_order(
@@ -74,4 +96,43 @@ pub async fn take_order(
     )
     .await;
 }
+pub async fn verify_balance_post_swap(
+    maker: &WalletUnlocked,
+    taker: &WalletUnlocked,
+    predicate_address: &Bech32Address,
+    order: LimitOrder,
+    provider: &Provider,
+) {
+    let maker_token = AssetId::from(order.maker_token.0);
+    let taker_token = AssetId::from(order.taker_token.0);
+    let balance_maker = maker.get_asset_balance(&taker_token).await.unwrap();
+    let balance_taker = taker.get_asset_balance(&maker_token).await.unwrap();
+    let predicate_balance = provider
+        .get_asset_balance(predicate_address, maker_token)
+        .await
+        .unwrap();
+    assert!(balance_maker == order.taker_amount);
+    assert!(balance_taker == order.maker_amount);
+    assert!(predicate_balance == 0);
+}
 
+// // for debugging purposes
+// async fn print_balances(
+//     maker: &WalletUnlocked,
+//     taker: &WalletUnlocked,
+//     predicate_address: &Bech32Address,
+//     provider: &Provider,
+//     coin: (u64, AssetId),
+// ) {
+//     let maker = maker.get_asset_balance(&coin.1).await.unwrap();
+//     let taker = taker.get_asset_balance(&coin.1).await.unwrap();
+//     let pred_b = provider
+//         .get_asset_balance(predicate_address, coin.1)
+//         .await
+//         .unwrap();
+//     println!("----------- COINS ----------");
+//     println!("Maker balance after: {:?}", maker);
+//     println!("Taker balance after: {:?}", taker);
+//     println!("Predicate balance after: {:?}", pred_b);
+//     println!("----------------------------");
+// }
