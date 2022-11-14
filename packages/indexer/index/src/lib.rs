ee174778f2da7e9e5a8d2c21380fa4ecde7a1219
jchen876 <jbchen876@gmail.com>
2022-11-14 11:48:16 -0800

    Indexer functionality, not finished

---
+++ packages/indexer/index/src/lib.rs
@@ -0,0 +1,22 @@
+extern crate alloc;
+use fuel_indexer_macros::{graphql_schema, handler};
+use fuels_abigen_macro::wasm_abigen;
+
+#[handler]
+fn handle_init(event: InitEvent) {
+    Logger::info("Make Created");
+    let InitEvent {
+        order
+    } = event;
+
+    let mut makeOrder = match Pool::load(contract_id) {
+        Some(t) => t,
+        None => Pool {
+            id: 0,
+          
+            swap_fee: swap_fee,
+        },
+    };
+
+    makeOrder.save();
+}
